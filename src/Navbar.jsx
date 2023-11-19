27dc1c9a6dc2195bbf17790e6c814636a92a2a70
Nathan <drejenathan06@gmail.com>
2023-11-19 11:56:54 +0300

    Initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,24 @@
+const Navbar = () => {
+  // const obj={
+  //   color: "white",
+  //           backgroundColor: "#35e4f1",
+  //           borderRadius: "8px",
+  // }
+  return (
+    <div className="navbar ">
+      <h1> Fun Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/">About Us</a>
+        <a
+          href="/create"
+          // style={obj}
+        >
+          New Blog
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
