4a5d30a12c6f0c0ce1016c39fdb60afc45cc1713
Nathan <drejenathan06@gmail.com>
2023-11-23 10:16:32 +0300

    Edited create, now it can add blogs directly in to the JSON file

--- dojo-blog/src/Create.jsx
+++ dojo-blog/src/Create.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Yoshi");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blogObject = { title, body, author };
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blogObject),
+    }).then(() => {
+      console.log("Added a new Blog!");
+    });
+  };
   return (
-    <div className='create'>
-        <h2>Add a new Blog!</h2>
+    <div className="create">
+      <h2>Add a new Blog!</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Mario">Mario</option>
+          <option value="Yoshi">Yoshi</option>
+        </select>
+
+        <button>Add Blog</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default Create;
